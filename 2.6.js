260009aea4228eeb89334ba4a5fb90a01db70ea0
Aurelien229 <blampainaurelien229@gmail.com>
2023-12-08 14:55:16 +0100

    save the code

---
+++ 2.6.js
@@ -0,0 +1,28 @@
+let nb =prompt("enter a number between 1 and 7");
+if ( nb == 1){
+    alert("Monday");
+}
+else
+    if ( nb == 2){
+        alert("Tuesday");
+    }
+else 
+    if ( nb == 3){
+        alert("Wednesday");
+    } 
+else
+    if ( nb == 4){
+        alert("Thursday");
+    } 
+else
+    if( nb == 5){
+        alert("Friday");
+    }  
+else
+    if( nb == 6){
+        alert("Saturday");
+    }  
+else
+    if( nb == 7){
+        alert("Sunday");
+    }           
